c973c72c03cc9b0b1e5f7e42e165052b93be7ed7
Lee wonho <lwh8989@naver.com>
2021-02-03 20:31:54 +0900

    addons update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,23 @@
+const path = require('path');
+
 module.exports = {
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+    return config;
+  },
+
   "stories": [
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@whitespace/storybook-addon-html",
+    "storybook-zeplin/register"
   ]
 }
